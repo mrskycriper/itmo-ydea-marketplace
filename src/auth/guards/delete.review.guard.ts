29538d2328023de1418fbc25ab607a987feb132b
Theodore Basmanov <chatta54@gmail.com>
2022-12-03 13:10:53 +0300

    Product module guards (+ minor fixes to other guards)

--- src/auth/guards/delete.review.guard.ts
+++ src/auth/guards/delete.review.guard.ts
@@ -1,8 +1,10 @@
 import {
+  BadRequestException,
   CanActivate,
   ExecutionContext,
   ForbiddenException,
   Injectable,
+  NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
 
@@ -10,7 +12,7 @@ import Session, { SessionContainer } from 'supertokens-node/recipe/session';
 import prisma from '../../client';
 
 @Injectable()
-export class AdminGuard implements CanActivate {
+export class DeleteReviewGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const httpContext = context.switchToHttp();
     let session: SessionContainer;
@@ -22,9 +24,22 @@ export class AdminGuard implements CanActivate {
     } catch (err) {
       throw new UnauthorizedException('Unauthorized');
     }
+    const path = httpContext.getRequest().path;
+    let reviewId: string;
+    try {
+      reviewId = path.split('/')[2];
+    } catch (err) {
+      throw new BadRequestException('Invalid review id');
+    }
+    const review = await prisma.review.findUnique({
+      where: { id: reviewId },
+    });
+    if (review == null) {
+      throw new NotFoundException('Review not found');
+    }
     const userId = session.getUserId();
     const user = await prisma.user.findUnique({ where: { id: userId } });
-    if (!user.is_admin && !user.is_moderator) {
+    if (!user.is_admin && !user.is_moderator && review.user_id != userId) {
       throw new ForbiddenException('Forbidden');
     }
 
--- src/auth/guards/editdelete.product.guard.ts
+++ src/auth/guards/editdelete.product.guard.ts
@@ -1,8 +1,10 @@
 import {
+  BadRequestException,
   CanActivate,
   ExecutionContext,
   ForbiddenException,
   Injectable,
+  NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
 
@@ -10,7 +12,7 @@ import Session, { SessionContainer } from 'supertokens-node/recipe/session';
 import prisma from '../../client';
 
 @Injectable()
-export class AdminGuard implements CanActivate {
+export class EditDeleteProductGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const httpContext = context.switchToHttp();
     let session: SessionContainer;
@@ -22,9 +24,25 @@ export class AdminGuard implements CanActivate {
     } catch (err) {
       throw new UnauthorizedException('Unauthorized');
     }
+    const path = httpContext.getRequest().path;
+    let productId: number;
+    try {
+      productId = Number.parseInt(path.split('/')[2]);
+    } catch (err) {
+      throw new BadRequestException('Invalid product id');
+    }
+    const product = await prisma.product.findUnique({
+      where: { id: productId },
+    });
+    if (product == null) {
+      throw new NotFoundException('Product not found');
+    }
+    const seller = await prisma.seller.findUnique({
+      where: { id: product.seller_id },
+    });
     const userId = session.getUserId();
     const user = await prisma.user.findUnique({ where: { id: userId } });
-    if (!user.is_admin && !user.is_moderator) {
+    if (!user.is_admin && !user.is_moderator && seller.user_id != userId) {
       throw new ForbiddenException('Forbidden');
     }
 
