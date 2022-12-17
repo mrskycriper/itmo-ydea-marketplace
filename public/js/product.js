c8375732cbc97f6d0cf75458cf1ad7e5da328e43
Leonid Vorontsov <leuri2001@gmail.com>
2022-12-17 16:11:03 +0300

    quickfix to remove unused code

--- public/js/product.js
+++ public/js/product.js
@@ -1,36 +1,3 @@
-async function changeProductImage() {
-  alert('I');
-}
-
-async function changeProductDescription() {
-  alert('D');
-}
-
-async function changeProductName() {
-  var nameField = document.getElementById('productNameField');
-  nameField.innerHTML =
-    '<form><input type=text id="newProductNameTextArea"> </input><button  id="newProductNameSubmitButton">Применить</button><span class="text-danger" id="changeProductNameResponse"></span></form>';
-  document
-    .getElementById('newProductNameSubmitButton')
-    .addEventListener('click', changeProductNameSubmit);
-}
-
-async function changeProductNameSubmit() {
-  event.preventDefault();
-  if (document.getElementById('newProductNameTextArea').value === '') {
-    return;
-  }
-  await _api.editProduct(
-    document.getElementById('newProductNameTextArea').value,
-    productDescription,
-    productPrice,
-    Number.parseInt(productNumber),
-    productCategoryID,
-    productID,
-  );
-  window.location.reload();
-}
-
 
 async function editProduct() {
   var nameFieldElement = document.getElementById('productNameField');
